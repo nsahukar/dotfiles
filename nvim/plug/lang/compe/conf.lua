9dde5ae66a93c3ae29bcff4ae01a7eb72aebaaf7
Nikhil Sahukar <nsahukar@gmail.com>
2021-07-01 00:22:57 +0530

    compe - Removed <TAB> mapping

--- nvim/plug/lang/compe/conf.lua
+++ nvim/plug/lang/compe/conf.lua
@@ -61,7 +61,7 @@ end
 
 vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm('<CR>')", {expr = true})
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+--vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+--vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
