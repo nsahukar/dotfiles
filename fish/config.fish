d59a453c6902e87b470f912a5213fab0b14c306f
Nikhil Sahukar <nsahukar@gmail.com>
2021-07-07 17:47:57 +0530

    Added ssh-agent config

--- fish/config.fish
+++ fish/config.fish
@@ -22,3 +22,12 @@ set PATH $PATH $GOROOT/bin $GOBIN $CARGOPATH $NGINXPATH
 #
 # Include hidden files in search (excluding some)
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden -g "!.git"'
+
+
+# SSH-AGENT
+#
+# if test -z (pgrep ssh-agent)
+#   eval (ssh-agent -c)
+#   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+#   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+# end
