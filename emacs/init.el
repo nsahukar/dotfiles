d8e02b196e829d08319d89e92675b42a9669a059
Nikhil Sahukar <nsahukar@gmail.com>
2021-07-21 13:51:38 +0530

    Added use-package - virtico, which-key, evil, modus-theme, magit

--- emacs/init.el
+++ emacs/init.el
@@ -17,9 +17,8 @@
 
 ;; Font
 (set-face-attribute 'default nil 
-					:font "mononoki Nerd Font" 
-					:height 140)
-
+  :font "mononoki Nerd Font" 
+  :height 140)
 
 
 ;; MELPA
@@ -36,32 +35,73 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
 
 ;; which-key
 (use-package which-key
-			 :config (which-key-mode))
+  :config (which-key-mode))
 
 
-;; ivy
-(use-package ivy
-			 :diminish
-			 :config (ivy-mode 1))
+;; vertico
+(use-package vertico
+  :init
+  (vertico-mode)
+  (setq vertico-cycle t))
 
 
 ;; evil-collection-issue #60 (Refer github)
 (setq evil-want-keybinding nil)
 ;; evil
 (use-package evil
-			 :config (evil-mode 1))
+  :config (evil-mode 1))
 ;; evil-collection
 (use-package evil-collection
-			 :after evil
-			 :init (evil-collection-init))
+  :after evil
+  :init (evil-collection-init))
+
+
+;; modus-themes
+(use-package modus-themes
+  :init
+  ;; Add all your customizations prior to laoding the themes
+  (setq modus-themes-italic-constructs t
+     modus-themes-bold-constructs nil
+     modus-themes-region '(bg-only no-extend)
+     modus-themes-mode-line '(moody accented borderless))
+  
+  ;; Load the theme files before enabling a theme (else you get an error)
+  (modus-themes-load-themes)
+  :config
+  ;; Load the theme of your choice:
+  (modus-themes-load-vivendi) ;; OR (modus-themes-load-operandi)
+  :bind ("<f5>" . modus-themes-toggle))
 
 
 ;; magit
 (use-package magit)
+
+
+;; diminish
+(use-package diminish
+  :config
+  (diminish 'auto-revert-mode)
+  (diminish 'which-key-mode)
+  (diminish 'evil-mode)
+  (diminish 'evil-collection-unimpaired-mode))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(diminish vertico modus-themes which-key use-package magit evil evil-collection)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
