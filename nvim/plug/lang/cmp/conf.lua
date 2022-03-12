4fdcda31710177395c37f7248bdd61bd327ec758
Nikhil Sahukar <nsahukar@gmail.com>
2022-03-13 00:41:35 +0530

    Fixed issues related to nvim-cmp config

--- nvim/plug/lang/cmp/conf.lua
+++ nvim/plug/lang/cmp/conf.lua
@@ -2,18 +2,9 @@ local cmp = require'cmp'
 local luasnip = require'luasnip'
 local lspkind = require'lspkind'
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			-- For `vsnip` user
-		  -- vim.fn["vsnip#anonymous"](args.body)
-
-			-- For `luasnip` user
 			luasnip.lsp_expand(args.body)
 		end,
 	},
@@ -34,10 +25,6 @@ cmp.setup({
 		['<Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.comlete()
 			else
 				fallback()
 			end
@@ -46,8 +33,6 @@ cmp.setup({
 		['<S-Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
 			else
 				fallback()
 			end
