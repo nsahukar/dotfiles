8628ce293ddd8077d79595061586da939b67b0fa
Nikhil Sahukar <nsahukar@gmail.com>
2022-03-24 00:11:32 +0530

    Added bspwm, sxhkd, and picom configurations

---
+++ bspwm/go-workspace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# okular
+okular '/home/nix/Documents/books/Go/Mihalis Tsoukalos - Mastering Go_ Harness the power of Go to build professional utilities and concurrent servers and services, 3rd Edition-Packt Publishing (2021).pdf' &
+sleep .2
+
+# kitty
+bspc rule -a kitty -o split_ratio=0.36
+kitty &
+
+# typora
+bspc node -f @parent
+bspc rule -a Typora -o split_ratio=0.6
+/usr/local/typora/Typora &
