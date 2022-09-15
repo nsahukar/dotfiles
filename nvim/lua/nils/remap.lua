c266b04deaa421a5fad53cdfb4e5611c95e7395f
Nikhil Sahukar <nsahukar@gmail.com>
2022-09-15 13:10:38 +0530

    Moving to cursor to end of selection after yank

--- nvim/lua/nils/remap.lua
+++ nvim/lua/nils/remap.lua
@@ -12,30 +12,41 @@ nnoremap('<M-j>', ':m .+1<CR>==')
 nnoremap('<M-k>', ':m .-2<CR>==')
 vnoremap("<M-j>", ":m '>+1<CR>gv=gv")
 vnoremap("<M-k>", ":m '<-2<CR>gv=gv")
+
 -- Switching between tabs
 nnoremap('<Tab>', ':tabnext<CR>')
 nnoremap('<S-Tab>', ':tabprev<CR>')
+
 -- Moving around split windows
 nnoremap('<C-h>', '<C-w>h')
 nnoremap('<C-j>', '<C-w>j')
 nnoremap('<C-k>', '<C-w>k')
 nnoremap('<C-l>', '<C-w>l')
+
 -- Copy, paste from system clipboard, using '+' register here
 vnoremap('<leader>y', '"+y')
 nnoremap('<leader>p', '"+p')
+-- Do not register the selection in clipboard
+-- when pasting on it
+xnoremap('<M-p>', '\"_dp')
+-- Move the cursor to the end of selection after a yank
+vnoremap('y', 'ygv<ESC>')
+
 -- Delete current word in insert mode
 inoremap('<M-BS>', '<C-w>')
 -- Stop highlighting (search)
 nnoremap('<leader><leader>', ':noh<CR>')
--- Do not register the selection in clipboard
--- when pasting on it
-xnoremap('<M-p>', '\"_dp')
 
 
 -- PLUGINS
--- 
+--
+-- NvimTree
+--
 -- Toggle nvim-tree
 nnoremap('<leader>e', ':NvimTreeToggle<CR>')
+
+-- Telescope
+--
 -- Telescope: find files
 nnoremap('<leader>ff', ':Telescope find_files<CR>')
 -- Telescope: find buffers
