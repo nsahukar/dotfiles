5a981b05dd54ff99b918632785c5defa28356121
Nikhil Sahukar <nsahukar@gmail.com>
2022-10-30 20:34:55 +0530

    default_capabilities in place of now deprecated update_capabilities

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -40,7 +40,7 @@ end
 -- `cmp-nvim-lsp` -> enhancing client's capability to make use of more (different)
 --                   completion results (candidates) provided by different
 --                   language servers.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 
 -- Configure following language servers with native LSP
