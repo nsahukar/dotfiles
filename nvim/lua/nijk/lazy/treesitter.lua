717557500527dbfe5d284a87d6883f2a95b2ae38
Nikhil Sahukar <nsahukar@gmail.com>
2024-02-03 21:52:07 +0530

    Was wrongly configured. Updated to right configuration

--- nvim/lua/nijk/lazy/treesitter.lua
+++ nvim/lua/nijk/lazy/treesitter.lua
@@ -1,71 +1,74 @@
 local M = {}
 
-M.opts = {
-  -- A list of parser names, or "all"
-  ensure_installed = {
-    'lua', 
-    'go', 
-    'rust', 
-    'javascript', 
-    'typescript', 
-    'html', 
-    'css', 
-    'c', 'cpp',
-    'dockerfile',
-    'make',
-    'yaml',
-    'vim', 
-    'fish'
-  },
-
-  -- Install parsers synchronously
-  -- (only applied to `ensure_installed`)
-  sync_install = true,
-
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = {},
-
-  highlight = {
-    enable = true,
-
-    -- list of language(s) to disable
-    disable = {}
-  },
-
-  indent = {
-    enable = false
-  },
-
-  refactor = {
-    highlight_definitions = {
-      enable = true
+M.config = function ()
+  local configs = require("nvim-treesitter.configs")
+  configs.setup({
+    -- A list of parser names, or "all"
+    ensure_installed = {
+      'lua',
+      'go',
+      'rust',
+      'javascript',
+      'typescript',
+      'html',
+      'css',
+      'c', 'cpp',
+      'dockerfile',
+      'make',
+      'yaml',
+      'vim',
+      'fish'
     },
-    smart_rename = {
+
+    -- Install parsers synchronously
+    -- (only applied to `ensure_installed`)
+    sync_install = true,
+
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = {},
+
+    highlight = {
       enable = true,
-      keymaps = {
-        smart_rename = "<leader>r"
+
+      -- list of language(s) to disable
+      disable = {}
+    },
+
+    indent = {
+      enable = false
+    },
+
+    refactor = {
+      highlight_definitions = {
+        enable = true
+      },
+      smart_rename = {
+        enable = true,
+        keymaps = {
+          smart_rename = "<leader>r"
+        }
       }
-    }
-  },
-}
+    },
+  })
+end
 
 M.treesitter = {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   name = "treesitter",
   build = ":TSUpdate",
-  opts = M.opts,
+  config = M.config,
 }
 
 M.treesitter_textobjects = {
-  "nvim-treesitter/nvim-treesitter-textobjects", 
+  "nvim-treesitter/nvim-treesitter-textobjects",
   dependencies = "treesitter"
 }
 
 M.treesitter_refactor = {
-  "nvim-treesitter/nvim-treesitter-refactor", 
+  "nvim-treesitter/nvim-treesitter-refactor",
   dependencies = "treesitter"
 }
 
