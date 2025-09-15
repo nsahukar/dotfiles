c97b2d420710996cef9cf5ca8bac5470f52b39e3
Nikhil Sahukar <nsahukar@gmail.com>
2025-09-16 00:27:58 +0530

    Last commit on old nvim config

---
+++ nvim/lua/nijk/lazy/flutter.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.opts = {}
+
+M.config = {}
+
+M.flutter = {
+  'akinsho/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim', -- optional for vim.ui.select
+  },
+  config = true,
+}
+
+return M.flutter
